1c62134370df84f1b420e31d0799eee7941120ba
wonjjani <wonjjani@gmail.com>
2025-10-04 16:29:03 +0900

    feat: s3-bucket collector

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from sqlalchemy import create_engine
+import pandas as pd
+
+app = FastAPI()
+
+# Steampipe PostgreSQL 연결
+engine = create_engine("postgresql://steampipe@localhost:9193/steampipe")
+
+@app.get("/s3-buckets")
+def get_s3_buckets():
+    query = """
+    select
+      name,
+      region,
+      creation_date
+    from
+      aws_s3_bucket
+    order by
+      region, name;
+    """
+    df = pd.read_sql(query, engine)
+    return df.to_dict(orient="records")
